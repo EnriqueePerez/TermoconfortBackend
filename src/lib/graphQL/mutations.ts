8958dab932c518071231e5e8f2425c3537745f12
Enrique Pérez <enrike_084@hotmail.com>
2021-03-09 16:05:06 -0600

    Added addTiendas and addUsuarios endpoints. SpecialTypesResolvers remove completely since it is no longer necessary, I already create parsers for timestamps and geopoints.

--- src/lib/graphQL/mutations.ts
+++ src/lib/graphQL/mutations.ts
@@ -1,4 +1,6 @@
 import { connectDB } from '../db/dbConnection';
+import firebase from 'firebase-admin';
+const { GeoPoint } = firebase.firestore;
 
 export = {
   addSobrecalentamiento: async (root: any, { input }) => {
@@ -45,5 +47,51 @@ export = {
     }
     return { ...input, ...id };
   },
-  addTienda: async (root: any, { input }) => {},
+  addTienda: async (root: any, { input }) => {
+    let newTienda = {
+      nombre: input.nombre,
+      ciudad: input.ciudad,
+      ubicacion: input.ubicacion,
+    };
+    let id = { id: 'sinId' };
+
+    //parsing ubicacion to a Geopoint object
+    const parsedUbicacion = new GeoPoint(
+      newTienda.ubicacion.latitude,
+      newTienda.ubicacion.longitude
+    );
+    //replacing ubicacion for the new Geopoint object
+    newTienda.ubicacion = parsedUbicacion;
+
+    try {
+      const db = await connectDB();
+      await db.collection('Tiendas').doc(`${input.id}`).set(newTienda);
+      // console.log('imprimiendo newTienda', newTienda);
+      id.id = input.id;
+    } catch (error) {
+      console.error('error en addTienda', error);
+    }
+    //returning the id and the info with the Geopoint object
+    return { ...newTienda, ...id };
+  },
+  addUsuario: async (root: any, { input }) => {
+    let newUsuario = {
+      nombre: input.nombre,
+      apellido: input.apellido,
+      email: input.email,
+      permisos: input.permisos,
+    };
+    //adding default data in case it is not existant
+    let id = { id: 'sinId' };
+
+    try {
+      const db = await connectDB();
+      await db.collection('Usuarios').doc(`${input.id}`).set(newUsuario);
+
+      id.id = input.id;
+    } catch (error) {
+      console.error('error en addUsuario', error);
+    }
+    return { ...newUsuario, ...id };
+  },
 };
--- src/lib/graphQL/specialTypesResolvers.ts
+++ src/lib/graphQL/specialTypesResolvers.ts
@@ -1,16 +1,1 @@
-import { GeoPoint } from '@google-cloud/firestore';
-
-export = {
-  Tienda: {
-    ubicacion: async ({ ubicacion }) => {
-      const latitude = ubicacion.latitude;
-      const longitude = ubicacion.longitude;
-
-      const parsedGeoPoint = new GeoPoint(latitude, longitude);
-      console.log(parsedGeoPoint);
-      //parsing GeoPoint to stringify
-      // const stringGeoPoint: String = JSON.stringify(ubicacion);
-      return parsedGeoPoint;
-    },
-  },
-};
+export = {};
